ac2ff7303e8d4924e73ded67bfda3cb4233386eb
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-03 23:07:26 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
+
+
+
+
+
+
 def square_array(array)
-  # your code here
+  new_array = []
+
+  array.each do |i|
+    new_array << i ** 2
+  end
+  return new_array
 end
+
+numbers = [1,2,3]
+ 
+square_array(numbers)
